3e22b3acc2009e4849583fc496d53e4d5b8ab266
Pavel <143266883+Pavel8913@users.noreply.github.com>
2024-01-23 20:12:15 +0700

    Add files via upload

---
+++ Pacman/Pacman/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Pacman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+       
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.level1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.game_Timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pacman = new System.Windows.Forms.PictureBox();
+            this.yellowGhost = new System.Windows.Forms.PictureBox();
+            this.pinkGhost = new System.Windows.Forms.PictureBox();
+            this.redGhost = new System.Windows.Forms.PictureBox();
+            this.wall4 = new System.Windows.Forms.PictureBox();
+            this.wall3 = new System.Windows.Forms.PictureBox();
+            this.wall2 = new System.Windows.Forms.PictureBox();
+            this.wall1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pacman)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowGhost)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pinkGhost)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redGhost)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ScoreLabel.Location = new System.Drawing.Point(1, 7);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(54, 27);
+            this.ScoreLabel.TabIndex = 8;
+            this.ScoreLabel.Text = "Score: ";
+            // 
+            // level1
+            // 
+            this.level1.BackColor = System.Drawing.Color.Blue;
+            this.level1.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.level1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.level1.Location = new System.Drawing.Point(137, 94);
+            this.level1.Name = "level1";
+            this.level1.Size = new System.Drawing.Size(200, 30);
+            this.level1.TabIndex = 9;
+            this.level1.Tag = "level";
+            this.level1.Text = "level1";
+            this.level1.UseVisualStyleBackColor = false;
+            this.level1.Click += new System.EventHandler(this.level1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(137, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Tag = "level";
+            this.button1.Text = "level2";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.level1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(137, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Tag = "level";
+            this.button2.Text = "level3";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.level1_Click_1);
+            // 
+            // game_Timer
+            // 
+            this.game_Timer.Tick += new System.EventHandler(this.game_Timer_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pacman
+            // 
+            this.pacman.BackColor = System.Drawing.Color.Transparent;
+            this.pacman.Cursor = System.Windows.Forms.Cursors.No;
+            this.pacman.Image = global::Pacman.Properties.Resources.pacman;
+            this.pacman.Location = new System.Drawing.Point(39, 76);
+            this.pacman.Name = "pacman";
+            this.pacman.Size = new System.Drawing.Size(40, 40);
+            this.pacman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pacman.TabIndex = 7;
+            this.pacman.TabStop = false;
+            this.pacman.Tag = "player";
+            // 
+            // yellowGhost
+            // 
+            this.yellowGhost.Image = global::Pacman.Properties.Resources.yellow_guy;
+            this.yellowGhost.Location = new System.Drawing.Point(251, 264);
+            this.yellowGhost.Name = "yellowGhost";
+            this.yellowGhost.Size = new System.Drawing.Size(53, 50);
+            this.yellowGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yellowGhost.TabIndex = 6;
+            this.yellowGhost.TabStop = false;
+            this.yellowGhost.Tag = "ghost";
+            // 
+            // pinkGhost
+            // 
+            this.pinkGhost.Image = global::Pacman.Properties.Resources.pinkghost;
+            this.pinkGhost.Location = new System.Drawing.Point(344, 161);
+            this.pinkGhost.Name = "pinkGhost";
+            this.pinkGhost.Size = new System.Drawing.Size(58, 50);
+            this.pinkGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pinkGhost.TabIndex = 5;
+            this.pinkGhost.TabStop = false;
+            this.pinkGhost.Tag = "ghost";
+            // 
+            // redGhost
+            // 
+            this.redGhost.Image = global::Pacman.Properties.Resources.red_guy;
+            this.redGhost.Location = new System.Drawing.Point(174, 56);
+            this.redGhost.Name = "redGhost";
+            this.redGhost.Size = new System.Drawing.Size(51, 48);
+            this.redGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.redGhost.TabIndex = 4;
+            this.redGhost.TabStop = false;
+            this.redGhost.Tag = "ghost";
+            // 
+            // wall4
+            // 
+            this.wall4.BackColor = System.Drawing.Color.Blue;
+            this.wall4.Location = new System.Drawing.Point(365, 250);
+            this.wall4.Name = "wall4";
+            this.wall4.Size = new System.Drawing.Size(27, 96);
+            this.wall4.TabIndex = 3;
+            this.wall4.TabStop = false;
+            this.wall4.Tag = "wall";
+            // 
+            // wall3
+            // 
+            this.wall3.BackColor = System.Drawing.Color.Blue;
+            this.wall3.Location = new System.Drawing.Point(173, 250);
+            this.wall3.Name = "wall3";
+            this.wall3.Size = new System.Drawing.Size(27, 96);
+            this.wall3.TabIndex = 2;
+            this.wall3.TabStop = false;
+            this.wall3.Tag = "wall";
+            // 
+            // wall2
+            // 
+            this.wall2.BackColor = System.Drawing.Color.Blue;
+            this.wall2.Location = new System.Drawing.Point(310, 0);
+            this.wall2.Name = "wall2";
+            this.wall2.Size = new System.Drawing.Size(27, 135);
+            this.wall2.TabIndex = 1;
+            this.wall2.TabStop = false;
+            this.wall2.Tag = "wall";
+            // 
+            // wall1
+            // 
+            this.wall1.BackColor = System.Drawing.Color.Blue;
+            this.wall1.Location = new System.Drawing.Point(141, 0);
+            this.wall1.Name = "wall1";
+            this.wall1.Size = new System.Drawing.Size(27, 135);
+            this.wall1.TabIndex = 0;
+            this.wall1.TabStop = false;
+            this.wall1.Tag = "wall";
+            this.wall1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.level1);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.pacman);
+            this.Controls.Add(this.yellowGhost);
+            this.Controls.Add(this.pinkGhost);
+            this.Controls.Add(this.redGhost);
+            this.Controls.Add(this.wall4);
+            this.Controls.Add(this.wall3);
+            this.Controls.Add(this.wall2);
+            this.Controls.Add(this.wall1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pacman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pacman)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowGhost)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pinkGhost)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redGhost)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wall1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox wall1;
+        private System.Windows.Forms.PictureBox wall2;
+        private System.Windows.Forms.PictureBox wall3;
+        private System.Windows.Forms.PictureBox wall4;
+        private System.Windows.Forms.PictureBox redGhost;
+        private System.Windows.Forms.PictureBox pinkGhost;
+        private System.Windows.Forms.PictureBox yellowGhost;
+        private System.Windows.Forms.PictureBox pacman;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Button level1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer game_Timer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pacman/Pacman/Form1.cs
@@ -0,0 +1,439 @@
+﻿#region кот
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+        bool goup,godown,goleft,goright;
+        int count_coin = 0, score;
+        int playerSpeed,redghostSpeed,yellowghostSpeed,pinkghostSpeed,pinkghostX,pinkghostY;
+        bool goPinkghost = true;
+        PictureBox[,] coin = new PictureBox[10, 9];
+
+        #region управление
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //switch(e.KeyCode)
+            //{
+            //    case Keys.W:goup= true; break;
+            //    case Keys.S:godown= true;break;
+            //    case Keys.A:goleft= true;break;
+            //    case Keys.D:goright= true;break;
+            //}
+            if (e.KeyCode == Keys.W)
+            {
+                goup = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                godown = true;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                goright = true;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+            if (e.KeyCode == Keys.F1)
+            {
+                score+= 70;
+            }   
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //switch (e.KeyCode)
+            //{
+            //    case Keys.W: goup = false; break;
+            //    case Keys.S: godown = false; break;
+            //    case Keys.A: goleft = false; break;
+            //    case Keys.D: goright = false; break;
+            //}
+
+            if (e.KeyCode == Keys.W)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                godown = false;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                goright = false;
+            }
+            if (e.KeyCode==Keys.Enter)
+            {
+                resetGame();
+                game_Timer.Stop();
+                timer1.Stop();
+            }
+            if(e.KeyCode==Keys.F5)
+            {
+                resetGame();
+                game_Timer.Stop();
+                timer1.Start();
+            }
+        }
+        #endregion
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ScoreLabel.Text="Score: "+score+Environment.NewLine+"All the coins: "+count_coin;
+            ScoreLabel.Visible=true;
+            if(goleft)
+            {
+
+                pacman.Left -= playerSpeed;
+                pacman.Image = Properties.Resources.pacman_left;
+            }
+            if(goright)
+            {
+                pacman.Left += playerSpeed;
+                pacman.Image = Properties.Resources.pacman;
+
+            }
+            if(goup)
+            {
+                pacman.Top-= playerSpeed;
+                pacman.Image= Properties.Resources.pacman_up;
+
+            }
+            if(godown)
+            {
+                pacman.Top += playerSpeed;
+                pacman.Image = Properties.Resources.pacman_down;
+            }
+            if (pacman.Left > this.Width)
+            {
+                pacman.Left = 0;
+            }
+            
+            if (pacman.Left > this.Width)
+            {
+                pacman.Left = 500;
+            }
+
+            if (pacman.Top > this.Height)
+            {
+                pacman.Top = 0;
+            }
+
+            if (pacman.Top < -40)
+            {
+                pacman.Top = 400;
+            }
+
+            if (pacman.Left < -40)
+            {
+                pacman.Left = 500;
+            }
+
+            if (score>=count_coin)
+            {
+                GameOvr("You Win!");
+                game_Timer.Stop();
+                timer1.Stop();
+            }
+
+            if (pinkGhost.Top < -40)
+            {
+                pinkghostX *= 1;
+            }
+
+
+            pinkGhost.Left -= pinkghostX;
+            pinkGhost.Top -= pinkghostY;
+
+            if(pinkGhost.Left<0||pinkGhost.Left>500)
+            {
+                pinkghostX *= -1;
+            }
+            if(pinkGhost.Top<0||pinkGhost.Top>400)
+            {
+                pinkghostY *= -1;
+            }
+
+           // pinkGhost.Left -= pinkghostX;
+           // pinkGhost.Top -= pinkghostY;
+           // if(goPinkghost)
+           // {
+           //     if (pinkGhost.Top < this.Top| pinkGhost.Top >this.Top)
+           //     {
+           //         pinkghostX *= -1;                    
+           //     }
+
+           //     if (pinkGhost.Width < this.Width || pinkGhost.Width >this.Width)
+           //     {
+           //         pinkghostX *= -1;
+           //     }
+           //     goPinkghost = false;
+           // }
+
+           //if(!goPinkghost)
+           //{
+           //     if (pinkGhost.Top > 0 || pinkGhost.Top < 400)
+           //     {
+           //         pinkghostX *= 1;
+                   
+           //     }
+
+           //     if (pinkGhost.Width < 0 || pinkGhost.Width > 500)
+           //     {
+           //         pinkghostX *= 1;
+           //     }
+           //     goPinkghost = true;
+           // }
+           
+           
+
+
+        }
+        private void game_Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "coin")
+                    {
+                        if (pacman.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            this.Controls.Remove(x);
+                            score++;
+                        }
+                    }
+
+
+
+                    if ((string)x.Tag == "wall")
+                    {
+                        if (pacman.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            GameOvr("You Lose!");
+                            timer1.Stop();
+                        }
+                        
+                            if (pinkGhost.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                pinkghostX *= -1;
+                            }
+                            redGhost.Left += redghostSpeed;
+                            if (redGhost.Bounds.IntersectsWith(wall1.Bounds) || redGhost.Bounds.IntersectsWith(wall2.Bounds))
+                            {
+                                redghostSpeed *= -1;
+                            }
+                            yellowGhost.Left += yellowghostSpeed;
+                            if (yellowGhost.Bounds.IntersectsWith(wall3.Bounds) || yellowGhost.Bounds.IntersectsWith(wall4.Bounds))
+                            {
+                                yellowghostSpeed *= -1;
+                            }
+
+
+                           
+
+
+                        }
+
+                    
+                }
+            }
+            foreach (Control y in this.Controls)
+            {
+                if (y is PictureBox)
+                {
+
+
+                    if ((string)y.Tag == "ghost")
+                    {
+                        if (pacman.Bounds.IntersectsWith(y.Bounds))
+                        {
+                            GameOvr("You Lose!");
+                            timer1.Stop();
+                        }
+                    }
+                }
+            }
+            //foreach (Control y in this.Controls)
+            //{
+            //    if (y is PictureBox)
+            //    {
+            //        if ((string)y.Tag == "ghost")
+            //        {
+
+            //        }
+            //    }
+            //}
+        }
+
+        private void level1_Click_1(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            foreach (Control x in this.Controls)
+            {
+                if (x is Button)
+                {
+                    x.Enabled = false;
+                    x.Visible = false;
+                }
+            }
+            generate_map1();
+            game_Timer.Start();
+            timer1.Start();
+            switch (bt.Text)
+            {
+                case "level1":
+                    {
+                        redghostSpeed = 5;
+                        yellowghostSpeed = 5;
+                        pinkghostX = 3;
+                        pinkghostY = 3;
+                        playerSpeed = 8;
+                        break;
+                    }
+
+
+                case "level2":
+                    {
+                        redghostSpeed = 7;
+                        yellowghostSpeed = 7;
+                        pinkghostX = 4;
+                        pinkghostY = 6;
+                        playerSpeed = 10;
+                        break;
+                    }
+
+
+                case "level3":
+                    {
+                        redghostSpeed = 9;
+                        yellowghostSpeed = 9;
+                        pinkghostX = 2;
+                        pinkghostY = 11;
+                        playerSpeed = 12;
+                        break;
+                    }
+
+
+            }
+        }
+
+        
+        
+        private void generate_map1()
+        {
+            
+            pacman.Left = 30;
+            pacman.Top = 30;
+
+            redGhost.Left= 215;
+            redGhost.Top = 60;
+
+            //251; 264
+            yellowGhost.Left= 251;
+            yellowGhost.Top= 264;
+
+            //344; 161
+            pinkGhost.Left= 344;
+            pinkGhost.Top = 161;
+
+            for(int i=0;i<10;i++)
+            {
+                for(int j=0;j<9;j++)
+                {
+                    coin[i, j] = new PictureBox();
+                    coin[i, j].Location = new Point(coin[i, j].Location.X + i * 50, coin[i,j].Location.Y+j*34);
+                    coin[i, j].Image = Properties.Resources.coin;
+                    coin[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    coin[i, j].Size = new Size(25,25);
+                    coin[i, j].Tag = "coin";
+                    count_coin++;
+                    this.Controls.Add(coin[i,j]);
+                    foreach(Control x in this.Controls)
+                    {
+                        if (coin[i,j].Bounds.IntersectsWith(x.Bounds)&&((string)x.Tag=="wall"))
+                        {
+                            this.Controls.Remove(coin[i,j]);
+                            count_coin--;
+                        }
+                        //if (coin[i,j].Bounds.IntersectsWith(x.Bounds)&&(string)x.Tag=="ghost")
+                        //{
+                        //    this.Controls.Remove(coin[i, j]);
+                        //    count_coin--;
+                        //}
+                    }
+                    
+                }
+            }
+            
+
+
+
+
+        }
+        private void resetGame()
+        {
+            timer1.Start();
+            foreach(Control x in this.Controls)
+            {
+                if(x is Button)
+                {
+                    x.Visible = true;
+                    x.Enabled= true;
+                }
+                for(int i=0;i<10;i++)
+                {
+                    for(int j=0;j<9;j++)
+                    {
+                        this.Controls.Remove(coin[i,j]);
+                    }
+                }
+
+                count_coin = 0;
+                score = 0;
+
+
+            }
+        }
+        private void GameOvr(string message)
+        {
+            game_Timer.Stop();
+            ScoreLabel.Text="Score: "+score+Environment.NewLine+message;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
+#endregion
